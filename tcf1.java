9e0280a4590c0365af32dfa6bb53151c3152af83
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-08 15:49:20 +0530

    Create tcf1.java

---
+++ tcf1.java
@@ -0,0 +1,23 @@
+/* try-catch-finally */
+class Handling
+{
+    public static  void main(String[] args) {
+        try 
+        {
+            System.out.println("Abhi Gupta");
+            int a=20,b=0,c;
+            c=a/b;
+            System.out.println(c);
+            System.out.println("like share");
+        }
+        catch(ArithmeticException a)
+        {
+            System.out.println("can't divide by zero");
+        }
+        finally
+        {
+            System.out.println("subscribe");
+        }
+        System.out.println("main method ended");
+    }
+}
